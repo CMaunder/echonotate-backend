7234c01f08f9d4305eee1a7ddf1f4afe2eaa0d95
Charlie Maunder <cmaunderc@gmail.com>
2021-04-07 18:52:20 +0100

    getting audiofiles from s3

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 from flask import Flask, jsonify
 import librosa
+import os
+from os import path
 from pydub import AudioSegment
+import boto3
+from decouple import config
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def get_notes():
+@app.route('/predicted-notes/<filename>')
+def get_notes(filename):
+    BUCKET_NAME_STRING='tabscribe-audio-store'
+    #TODO handle wav, m4a
+    tempfilename = './tempSoundFiles/tempfilename.mp3'
+
+    s3_client = boto3.client('s3', aws_access_key_id=config('AWS_ACCESS_KEY_ID_TS'), aws_secret_access_key=config('AWS_SECRET_ACCESS_KEY_TS'))
+    s3_client.download_file(BUCKET_NAME_STRING, filename, tempfilename)
+
     # assign files
-    input_file = "./c-major-scale.mp3"
-    output_file = "./c-major-scale.wav"
+    output_file = "./tempSoundFiles/tempfilename.wav"
 
     # convert mp3 file to wav file
-    sound = AudioSegment.from_mp3(input_file)
+    sound = AudioSegment.from_mp3(tempfilename)
     sound.export(output_file, format="wav")
     a, sr = librosa.load(output_file)
-    return jsonify({"lengthOfFile 2.wav": len(a)})
+    return jsonify({f"lengthOfFile {filename}": len(a)})
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,14 +1,23 @@
 import librosa
+import os
 from os import path
 from pydub import AudioSegment
+import boto3
+from decouple import config
 
+BUCKET_NAME_STRING='tabscribe-audio-store'
+FILE_NAME_STRING='d-flat-ionian-mode-on-treble-clef.mp3'
+
+tempfilename = './tempfilename.mp3'
+
+s3_client = boto3.client('s3', aws_access_key_id=config('AWS_ACCESS_KEY_ID_TS'), aws_secret_access_key=config('AWS_SECRET_ACCESS_KEY_TS'))
+s3_client.download_file(BUCKET_NAME_STRING, FILE_NAME_STRING, tempfilename)
 
 # assign files
-input_file = "./c-major-scale.mp3"
 output_file = "./c-major-scale.wav"
 
 # convert mp3 file to wav file
-sound = AudioSegment.from_mp3(input_file)
+sound = AudioSegment.from_mp3(tempfilename)
 sound.export(output_file, format="wav")
 a, sr = librosa.load(output_file)
 print(len(a))
