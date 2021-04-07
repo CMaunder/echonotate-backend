9ca81e8ffbb822db1428b0d327211965d2344238
Charlie Maunder <charlie.maunder@fdmgroup.com>
2021-04-07 12:17:58 +0100

    convertion for mp3 testing

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_notes():
-    filename = './2.wav'
-    a, sr = librosa.load(filename)
+    # assign files
+    input_file = "./c-major-scale.mp3"
+    output_file = "./c-major-scale.wav"
+
+    # convert mp3 file to wav file
+    sound = AudioSegment.from_mp3(input_file)
+    sound.export(output_file, format="wav")
+    a, sr = librosa.load(output_file)
     return jsonify({"lengthOfFile 2.wav": len(a)})
 
 
--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 import librosa
+from os import path
+from pydub import AudioSegment
 
-filename = './2.wav'
-a, sr = librosa.load(filename)
+
+# assign files
+input_file = "./c-major-scale.mp3"
+output_file = "./c-major-scale.wav"
+
+# convert mp3 file to wav file
+sound = AudioSegment.from_mp3(input_file)
+sound.export(output_file, format="wav")
+a, sr = librosa.load(output_file)
 print(len(a))
