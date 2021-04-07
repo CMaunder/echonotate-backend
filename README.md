e4d70f098d4677dc82589059ff468f651aa3d563
Charlie Maunder <charlie.maunder@fdmgroup.com>
2021-04-07 11:45:42 +0100

    adding librosa

---
+++ README.md
@@ -0,0 +1,6 @@
+# To deploy to production
+
+- push changes via git
+- log into server with: ssh -i tabscribe-predict-notes.pem ubuntu@ec2-18-130-228-3.eu-west-2.compute.amazonaws.com
+- ensure in root of directory, then git pull
+- then: sudo systemctl restart tabscribe-predict-notes
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify
+import librosa
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_notes():
-    return jsonify({"about": "notes go here"})
+    filename = './c-major-scale.mp3'
+    a, sr = librosa.load(filename)
+    return jsonify({"lengthOfFile": len(a)})
 
 
 if __name__ == "__main__":
